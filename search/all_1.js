da4d7b491ac87524ed96251e626204f2bcc91d5f
Senbei64 <fabio.carignano@gmail.com>
2026-04-09 06:22:44 +0000

    Deploying to gh-pages from @ Senbei64/blink65@46d4d703dec843ebcb33785a299b230b4fc839cd 🚀

--- search/all_1.js
+++ search/all_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['api_0',['Arduino-style API',['../index.html#autotoc_md1',1,'']]],
-  ['arduino_20style_20api_1',['Arduino-style API',['../index.html#autotoc_md1',1,'']]]
+  ['acknowledgements_0',['Acknowledgements',['../index.html#autotoc_md3',1,'']]],
+  ['api_1',['Arduino-style API',['../index.html#autotoc_md1',1,'']]],
+  ['arduino_20style_20api_2',['Arduino-style API',['../index.html#autotoc_md1',1,'']]]
 ];
